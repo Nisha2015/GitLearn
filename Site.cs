d9481cc80936c963678e8e0df3413e086b16c999
Gambhir, Nisha <ngambhir@dovercorp.com>
2018-01-02 14:39:36 +0530

    first commit

---
+++ Site.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLearn
+{
+    public class Site
+    {
+        private Guid Id;
+        public string name;
+        public int NoofTerminals;
+
+    }
+}
---
+++ Terminal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLearn
+{
+    public class Terminal : ITerminal
+    {
+        public Task<int> GetTerminalId()
+        {
+            return Task.FromResult<int>(5);
+        }
+
+        public Task<string> GetTerminalName()
+        {
+            return Task.FromResult<string>("TestStr");
+        }
+    }
+}
